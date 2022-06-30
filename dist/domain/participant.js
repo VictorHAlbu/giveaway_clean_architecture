b3ac8c0400321e8210a5cf7079c1d4f132808e6f
VictorHAlbu <vh12albuquerque@gmail.com>
2022-06-30 11:39:14 -0300

    config jest

---
+++ dist/domain/participant.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Participant = void 0;
+class Participant {
+    constructor(name, email, score) {
+        this.name = name;
+        this.email = email;
+        this.score = score;
+    }
+}
+exports.Participant = Participant;
---
+++ dist/domain/participant.spec.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const participant_1 = require("./participant");
+describe('Participant', () => {
+    test('should create participant', () => {
+        const participant = new participant_1.Participant('otavio', 'otavio@mail.com', 10);
+        expect(participant.name).toBe('otavio');
+        expect(participant.email).toBe('otavio@mail.com');
+        expect(participant.score).toBe(10);
+    });
+});
