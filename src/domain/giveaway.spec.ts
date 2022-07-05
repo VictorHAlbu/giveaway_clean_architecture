64181e50973c4099f022ef9514b4c56da9372597
Victor Hugo Albuquerque <vh12albuuerque@gmail.com>
2022-07-04 21:18:12 -0300

    feat: add GiveAway draw function

--- src/domain/giveaway.spec.ts
+++ src/domain/giveaway.spec.ts
@@ -20,4 +20,24 @@ describe('Giveaway', () => {
     giveaway.addParticipant(participant)
     expect(giveaway.participants).not.toContain(participant)
   })
+
+
+  test('should draw random participant', () => {
+
+    const participant1 = new Participant('otavio', 'otavio@mail.com', 10)
+    const participant2 = new Participant('hugo', 'hugo@mail.com', 9)
+    const participant3 = new Participant('victor', 'victor@mail.com', 8)
+
+    const giveaway = new GiveAway(8)
+
+    giveaway.addParticipant(participant1)
+    giveaway.addParticipant(participant2)
+    giveaway.addParticipant(participant3)
+
+    const drawn = giveaway.draw()
+    expect(drawn === participant1 || drawn === participant2 || drawn === participant3).toBeTruthy()
+    console.log(drawn)
+  })
+
+
 })
